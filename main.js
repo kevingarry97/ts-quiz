99a69b11de7f73d5b5a372e1b9a24b132d9c8473
Kevin ISHIMWE <kevin@techaffinity.com>
2022-01-05 22:32:36 +0200

    Candy

---
+++ main.js
@@ -0,0 +1,5 @@
+var printCandy = function (number) {
+    if (+number % 2 === 0)
+        console.log('candy');
+};
+printCandy(3);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let printCandy = (number: Number) => {
+    if(+number % 2 === 0) 
+        console.log('candy')
+}
+
+printCandy(3);
