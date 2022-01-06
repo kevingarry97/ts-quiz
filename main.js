6f40cb57ebde7d9766b44a284eb5c01024c56452
Kevin ISHIMWE <kevin@techaffinity.com>
2022-01-06 21:59:50 +0200

    Created a Display function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var printCandy = function (number) {
+var display = function (number) {
+    if (+number < 0)
+        return;
     if (+number % 2 === 0)
-        console.log('candy');
+        return console.log('candy');
+    if (+number % 11 === 0)
+        return console.log('bar');
+    if (+number % 22 === 0)
+        return console.log('candybar');
+    return console.log(number);
 };
-printCandy(3);
+display(11);
--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-let printCandy = (number: Number) => {
-    if(+number % 2 === 0) 
-        console.log('candy')
+let display = (number: Number) => {
+    if(+number < 0) return;
+
+    if(+number % 2 === 0) return console.log('candy')
+
+    if(+number % 11 === 0) return console.log('bar')
+
+    if(+number % 22 === 0) return console.log('candybar')
+
+    return console.log(number)
 }
 
-printCandy(3);
+display(11);
